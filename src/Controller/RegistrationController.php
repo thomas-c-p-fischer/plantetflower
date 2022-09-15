84143baa4316bbde66dc8ebfd457c8dc599028ef
thomas-c-p-fischer <thomas.fischermbaka@gmail.com>
2022-09-15 15:17:19 +0200

    Vf-authentification
    - Création du système d'authentification via la commande Symfony console make:auth
    
    - Création du formulaire d'enregistrement (uniquement la base -> email, roles[], password "hacher", is_verified, created_at (uniquement la date), my_purchases, my_sales), de l'envoi du mail après validation du formulaire pour confirmation du mail (réception possible dans les spams) et de la redirection  vers le SecurityController
    
    - Modifications des noms de routes dans les contrôleurs générés (remplacement des "App_ par les noms de controlleur")

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use App\Security\EmailVerifier;
+use Doctrine\ORM\EntityManagerInterface;
+use http\Client;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
+
+class RegistrationController extends AbstractController
+{
+    private EmailVerifier $emailVerifier;
+
+    public function __construct(EmailVerifier $emailVerifier)
+    {
+        $this->emailVerifier = $emailVerifier;
+    }
+
+    #[Route('/register', name: 'registration_register')]
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->date = new \DateTime('now');
+            $user->setCreatedAt($this->date);
+            // encoder le mot de passe
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            // générer une url signée et l’envoyer à l’utilisateur
+            $this->emailVerifier->sendEmailConfirmation('verify_email', $user,
+                (new TemplatedEmail())
+                    ->from(new Address('myflowerlifeantigaspi@gmail.com', 'Plant & Flower'))
+                    ->to($user->getEmail())
+                    ->subject('Please Confirm your Email')
+                    ->htmlTemplate('registration/confirmation_email.html.twig')
+            );
+            // faire tout ce dont vous avez besoin ici, comme envoyer un email
+
+            return $this->redirectToRoute('security_login');
+        }
+
+        return $this->render('registration/register.html.twig', [
+            'registrationForm' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/verify/email', name: 'verify_email')]
+    public function verifyUserEmail(Request $request): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        // valider le lien de confirmation par e-mail, sets User::isVerified=true and persists
+        try {
+            $this->emailVerifier->handleEmailConfirmation($request, $this->getUser());
+        } catch (VerifyEmailExceptionInterface $exception) {
+            $this->addFlash('verify_email_error', $exception->getReason());
+
+            return $this->redirectToRoute('registration_register');
+        }
+
+        // @TODO Change the redirect on success and handle or remove the flash message in your templates
+        $this->addFlash('success', 'Your email address has been verified.');
+
+        return $this->redirectToRoute('registration_register');
+    }
+}
