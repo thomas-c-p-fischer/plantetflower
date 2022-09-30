2440e0424595dd75da83bd06be4e6bc382996dab
Thomas Fischer <92039475+thomas-c-p-fischer@users.noreply.github.com>
2022-09-30 14:19:53 +0100

    Delete MangoPaySdkStorage.tmp.php

--- public/MangoPaySdkStorage.tmp.php
+++
@@ -1,3 +0,0 @@
-
-<?php exit(); ?>O:29:"MangoPay\Libraries\OAuthToken":5:{s:11:"create_time";i:1664458275;s:12:"access_token";s:32:"6d8953792eb348c3868b56d0bb98f4de";s:10:"token_type";s:6:"bearer";s:10:"expires_in";i:1199;s:17:"autentication_key";s:32:"295d45433677d078b1abd4633b77e96c";}
-
