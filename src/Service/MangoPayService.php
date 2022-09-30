f118b9d323617312ec93cf3d1896de2392b6bf8c
Thomas Fischer <92039475+thomas-c-p-fischer@users.noreply.github.com>
2022-09-30 14:15:44 +0100

    Update MangoPayService.php

--- src/Service/MangoPayService.php
+++ src/Service/MangoPayService.php
@@ -4,11 +4,14 @@ namespace App\Service;
 
 use App\Entity\User;
 use MangoPay;
+use MangoPay\Libraries\Exception;
+use MangoPay\Wallet;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
 
 class MangoPayService
 {
     private MangoPay\MangoPayApi $mangoPayApi;
-
 
     public function __construct()
     {
@@ -17,21 +20,42 @@ class MangoPayService
         $this->mangoPayApi->Config->ClientPassword = $_ENV['API_KEY'];
         $this->mangoPayApi->Config->BaseUrl = 'https://api.sandbox.mangopay.com';
         $this->mangoPayApi->Config->TemporaryFolder = $_ENV['TMP_PATH'];
-
     }
 
-
-    public function createNaturalUser($firstName, $lastName, $email)
+//Methode permettant de creer un utlisateur sur MangoPay
+    public function createNaturalUser(User $user)
     {
+        $mangoPayApi = $this->mangoPayApi;
         $newUser = new \MangoPay\UserNatural();
-        $newUser->Email = $email;
-        $newUser->FirstName = $firstName;
-        $newUser->LastName = $lastName;
-        $newUser->Birthday = 121271;
-        $newUser->Nationality = "GB";
-        $newUser->CountryOfResidence = "FR";
-        $result = $this->mangoPayApi->Users->Create($newUser);
+        $newUser->Email = $user->getEmail();
+        $newUser->FirstName = $user->getFirstName();
+        $newUser->LastName = $user->getLastName();
+        $newUser->Birthday = $user->getBirthday()->getTimestamp();
+        $newUser->Nationality = $user->getNationality();
+        $newUser->CountryOfResidence = $user->getCountryOfResidence();
+        if ($user->isOwner()) {
+            $newUser->UserCategory = "Owner";
+        } else {
+            $newUser->UserCategory = "Payer";
+        }
+        $result = $mangoPayApi->Users->Create($newUser);
+        $idMangoPay = $result->Id;
+        $user->setIdMangopay($idMangoPay);
         return $result->Id;
     }
 
+
+//Methode permettant de creer un wallet a un natural user
+    public function createWalletForNaturalUser($naturalUserId, User $user)
+    {
+
+        $Wallet = new Wallet();
+        $Wallet->Owners = array($naturalUserId);
+        $Wallet->Description = $user->getFirstName() . ' ' . $user->getLastName() . ' wallet' . '.';
+        $Wallet->Currency = "EUR";
+        $result = $this->mangoPayApi->Wallets->Create($Wallet);
+        $idWallet = $result->Id;
+        $user->setidWallet($idWallet);
+        return $result->Id;
+    }
 }
