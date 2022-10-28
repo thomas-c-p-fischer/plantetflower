e95e9530fa07cb46092804b33e57d017459cfdea
valentin <v.folastre@gmail.com>
2022-10-28 16:58:28 +0200

    vf-global_front
    - Correction des éléments suite à la réunion

--- public/assets/Javascript/app.js
+++ public/assets/Javascript/app.js
@@ -1,13 +1,14 @@
 // Script permettant de d'afficher la liste de pages dans un menu déroulant
 // de la barre de navigation de base.html.twig.
 // Recuperation des elements par Id qu'on initialise dans une variable.
-let containerMenuBtn = document.getElementById("container-hamburger-menu");
 let menuBtn = document.getElementById("hamburger-menu");
 let pagesList = document.getElementById("list-mains_pages");
 let secondaryUserPage = document.getElementById("container-dropdown-secondary_user_page");
+let responsive;
 
 // Event de type clic pour afficher et cacher la listes des pages.
 menuBtn.addEventListener("click", () => {
+    responsive = true;
     if (pagesList.style.display !== 'flex') {
         pagesList.style.display = 'flex';
         secondaryUserPage.style.display = 'flex';
@@ -16,3 +17,11 @@ menuBtn.addEventListener("click", () => {
         secondaryUserPage.style.display = 'none';
     }
 })
+
+// Event de type clic pour cacher la listes des pages si l'utilisateur clique ailleurs.
+$(document.body).click(function(e) {
+    if( !$.contains(menuBtn,e.target) && responsive === true ) {
+        pagesList.style.display = 'none';
+        secondaryUserPage.style.display = 'none';
+    }
+});
